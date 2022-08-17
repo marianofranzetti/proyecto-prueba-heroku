fbe1575a3bf6ce0b0fb2936ca1fd744a06874e9b
Mariano Franzetti <marianofranzetti@outlook.com.com>
2022-08-16 23:55:18 -0300

    cambios

--- src/test/java/com/example/demo/JejeApplicationTests.java
+++ src/test/java/com/example/demo/JejeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NuevoApplicationTests {
+class JejeApplicationTests {
 
 	@Test
 	void contextLoads() {
