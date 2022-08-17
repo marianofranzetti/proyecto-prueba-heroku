de27cff09398f4e562f345533ad37a122a248d4a
Mariano Franzetti <marianofranzetti@outlook.com.com>
2022-08-16 23:46:40 -0300

    cambios

---
+++ src/main/java/com/example/demo/NuevoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuevoApplication.class, args);
+	}
+
+}
